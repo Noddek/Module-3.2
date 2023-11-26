95f13e23e8621a31d9e44ebc54978d7d99945fd9
Noddek <4deeplearning@gmail.com>
2023-11-26 22:00:34 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿namespace VarConstLit
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            string MyName;
+            MyName = "Almaz";
+            Console.WriteLine(MyName);
+            Console.ReadKey();
+
+        }
+    }
+}
